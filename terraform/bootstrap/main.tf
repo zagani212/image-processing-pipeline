9f3cbbc0885f84ce8e9de5c28cec27ce3187c90a
zagani212 <abdelhakassahi@gmail.com>
2026-04-19 19:31:11 +0200

    bootstrap folder

---
+++ terraform/bootstrap/main.tf
@@ -0,0 +1,42 @@
+resource "aws_s3_bucket" "tf_state" {
+  bucket = format("my-terraform-state-bucket-%s-%s", var.account_id, var.region)
+  tags = {
+    Name        = "Terraform State Bucket"
+  }
+}
+
+# Versionning
+resource "aws_s3_bucket_versioning" "tf_state_versioning" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Server side encryption SSE-S3
+resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state_encryption" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "tf_lock" {
+  name         = "terraform-state-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "Terraform Lock Table"
+    Environment = "global"
+  }
+}
