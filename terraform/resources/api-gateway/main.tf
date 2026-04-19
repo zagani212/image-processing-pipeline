7d0f2227a24bc48e6fd6e3d0a14c1f371108c3b3
zagani212 <abdelhakassahi@gmail.com>
2026-04-19 20:21:10 +0200

    add dev stage for api gateway

--- terraform/resources/api-gateway/main.tf
+++ terraform/resources/api-gateway/main.tf
@@ -2,3 +2,9 @@ resource "aws_apigatewayv2_api" "api_gw" {
   name          = "image-processing-api"
   protocol_type = "HTTP"
 }
+
+resource "aws_apigatewayv2_stage" "dev_stage" {
+  api_id = aws_apigatewayv2_api.api_gw.id
+  auto_deploy = true
+  name   = "dev-stage"
+}
---
+++ terraform/resources/output.tf
@@ -0,0 +1,3 @@
+output "invoke_url" {
+  value = module.api_gw.invoke_url
+}
