289af3d6438e7324a1c308d73cfb1304650b6eaf
zagani212 <abdelhakassahi@gmail.com>
2026-04-24 17:37:08 +0200

    fix api_gw issues

--- terraform/resources/api-gateway/integrations.tf
+++ terraform/resources/api-gateway/integrations.tf
@@ -2,6 +2,7 @@ resource "aws_apigatewayv2_integration" "example" {
   api_id           = aws_apigatewayv2_api.api_gw.id
   integration_type = "AWS_PROXY"
 
+  payload_format_version = "2.0"
   description               = "validate image"
   integration_method        = "POST"
   integration_uri           = var.validate
--- terraform/resources/api-gateway/output.tf
+++ terraform/resources/api-gateway/output.tf
@@ -1,3 +1,7 @@
 output "invoke_url" {
   value = aws_apigatewayv2_stage.dev_stage.invoke_url
 }
+
+output "api_gw_arn" {
+  value = aws_apigatewayv2_api.api_gw.execution_arn
+}
