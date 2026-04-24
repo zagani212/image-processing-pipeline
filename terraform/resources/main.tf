24932881cfcc00e9fcc9282a12b654a980df2efc
zagani212 <abdelhakassahi@gmail.com>
2026-04-24 23:04:52 +0200

    adapt main.tf

--- terraform/resources/main.tf
+++ terraform/resources/main.tf
@@ -23,12 +23,20 @@ module "sqs" {
   source = "./sqs"
 }
 
+module "sns" {
+  source = "./sns"
+
+  thumbnail_sqs_arn = module.sqs.thumbnail_sqs_arn
+  medium_sqs_arn = module.sqs.medium_sqs_arn
+}
+
 module "lambda" {
   source = "./lambda"
 
   metadata_queue_url = module.sqs.metadata_queue_url
   images_bucket = module.s3.bucket_name
   dynamo_table = module.dynamodb.images_table
+  sns_arn = module.sns.sns_arn
 }
 
 module "api_gw" {
@@ -43,10 +51,14 @@ module "permission" {
   validate_function = "validate"
   metadata_function_arn = module.lambda.metadata
   api_gw = module.api_gw.api_gw_arn
-  sqs_arn = module.sqs.metadata_sqs_arn
+  sns_arn = module.sns.sns_arn
   bucket_arn = module.s3.bucket_arn
   dynamo_table_arn = module.dynamodb.images_table_arn
   metadata_sqs_arn = module.sqs.metadata_sqs_arn
+  thumbnail_sqs_arn = module.sqs.thumbnail_sqs_arn
+  thumbnail_sqs_url = module.sqs.thumbnail_sqs_url
+  medium_sqs_arn = module.sqs.medium_sqs_arn
+  medium_sqs_url = module.sqs.medium_sqs_url
   validate_role_name = "validate_function_role"
   metadata_role_name = "metadata_function_role"
 }
