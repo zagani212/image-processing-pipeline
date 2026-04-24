0a4e63a956a7f3419396a61c194a0a74ad322aac
zagani212 <abdelhakassahi@gmail.com>
2026-04-24 21:13:14 +0200

    adjust api gateway

--- terraform/resources/api-gateway/main.tf
+++ terraform/resources/api-gateway/main.tf
@@ -1,6 +1,15 @@
 resource "aws_apigatewayv2_api" "api_gw" {
   name          = "image-processing-api"
   protocol_type = "HTTP"
+
+  cors_configuration {
+    allow_credentials = false
+    allow_headers = ["authorization", "*"]
+    allow_methods = ["*"]
+    allow_origins = ["http://localhost:5173"]
+    expose_headers = ["date", "x-api-id", "*"]
+    max_age = "0"
+  }
 }
 
 resource "aws_apigatewayv2_stage" "dev_stage" {
