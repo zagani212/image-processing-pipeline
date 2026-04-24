e5aeeed42b1dfd4c0ea1c8f5a43d789d706b9012
zagani212 <abdelhakassahi@gmail.com>
2026-04-24 23:04:16 +0200

    add 2 new sqs

--- terraform/resources/sqs/main.tf
+++ terraform/resources/sqs/main.tf
@@ -2,3 +2,13 @@ resource "aws_sqs_queue" "metadata_queue" {
   name                      = "images-metadata-queue"
   delay_seconds             = 0
 }
+
+resource "aws_sqs_queue" "thumbnail_queue" {
+  name                      = "images-thumbnail-queue"
+  delay_seconds             = 0
+}
+
+resource "aws_sqs_queue" "medium_queue" {
+  name                      = "images-medium-queue"
+  delay_seconds             = 0
+}
--- terraform/resources/sqs/output.tf
+++ terraform/resources/sqs/output.tf
@@ -5,3 +5,27 @@ output "metadata_queue_url" {
 output "metadata_sqs_arn" {
   value = aws_sqs_queue.metadata_queue.arn
 }
+#####
+output "thumbnail_queue_url" {
+  value = aws_sqs_queue.thumbnail_queue.url
+}
+
+output "thumbnail_sqs_arn" {
+  value = aws_sqs_queue.thumbnail_queue.arn
+}
+
+output "thumbnail_sqs_url" {
+  value = aws_sqs_queue.thumbnail_queue.url
+}
+####
+output "medium_queue_url" {
+  value = aws_sqs_queue.medium_queue.url
+}
+
+output "medium_sqs_arn" {
+  value = aws_sqs_queue.medium_queue.arn
+}
+
+output "medium_sqs_url" {
+  value = aws_sqs_queue.medium_queue.url
+}
