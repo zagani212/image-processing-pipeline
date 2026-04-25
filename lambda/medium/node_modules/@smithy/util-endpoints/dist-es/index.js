dc870e58130475bf374a64d7ff9f4e66ed5b47fd
zagani212 <abdelhakassahi@gmail.com>
2026-04-25 14:50:27 +0200

    remove node_modules

--- lambda/medium/node_modules/@smithy/util-endpoints/dist-es/index.js
+++
@@ -1,8 +0,0 @@
-export { BinaryDecisionDiagram } from "./bdd/BinaryDecisionDiagram";
-export * from "./cache/EndpointCache";
-export { decideEndpoint } from "./decideEndpoint";
-export * from "./lib/isIpAddress";
-export * from "./lib/isValidHostLabel";
-export * from "./utils/customEndpointFunctions";
-export * from "./resolveEndpoint";
-export * from "./types";
